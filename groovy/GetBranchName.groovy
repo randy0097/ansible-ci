82bfcef5818457e63151d5a4b9d7849adb89d6c3
Randy <randy.xia98@gmail.com>
2018-02-23 14:24:10 +0800

    暂时去掉前后端分支选择项

--- groovy/GetBranchName.groovy
+++ groovy/GetBranchName.groovy
@@ -1,17 +1,19 @@
-if (this.args.size() !=1 )  {
-	throw new IllegalArgumentException('this script expects exactly one arguments')
-}
+// if (this.args.size() !=1 )  {
+// 	throw new IllegalArgumentException('this script expects exactly one arguments')
+// }
 
-def gitUrl = ''
-def frontGitUrl = 'git@192.168.194.114:iscloud-frontend/iscloud-web.git'
-def backGitUrl = 'git@192.168.194.114:iscloud-backend/iscloud.git'
-if(this.args[0] == 'frontend') {
-	gitUrl = frontGitUrl
-}else if(this.args[0] == 'backend') {
-	gitUrl = backGitUrl
-}else {
-	throw new IllegalArgumentException('argument value is wrong')
-}
+// def gitUrl = ''
+// def frontGitUrl = 'git@192.168.194.114:iscloud-frontend/iscloud-web.git'
+// def backGitUrl = 'git@192.168.194.114:iscloud-backend/iscloud.git'
+// if(this.args[0] == 'frontend') {
+// 	gitUrl = frontGitUrl
+// }else if(this.args[0] == 'backend') {
+// 	gitUrl = backGitUrl
+// }else {
+// 	throw new IllegalArgumentException('argument value is wrong')
+// }
+
+def gitUrl = 'git@192.168.194.114:iscloud-frontend/iscloud-web.git'
 
 def newList = []
 def branches = ("git ls-remote -h $gitUrl").execute()
