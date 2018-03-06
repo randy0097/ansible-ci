a252f5030ef90a132428e54c6dba33428fde3296
Randy <randy.xia98@gmail.com>
2018-03-06 14:21:57 +0800

    prod环境脚本优化

--- groovy/GetProdEnvName.groovy
+++ groovy/GetProdEnvName.groovy
@@ -3,8 +3,8 @@ def newList = []
 lineList = new File(hostDir).readLines();
 lineList.each {
 	(it =~ /\[.+\]/).each{ match ->
-		//去掉组变量定义和生产环境配置
-		if(!match.contains(":vars")&&!match.contains("web_prod_real")){
+		//获取生产环境配置
+		if(match.contains("web_prod_real")){
 			newList += match.replaceAll(/\[/,'').replaceAll(/\]/,'')
 		}
 	}
